0e1d12c23c2c6b06c3033be939ac31d1e228ab07
Gaston Dario Napoli <gaston.napoli@gmail.com>
2019-03-16 22:09:15 -0300

    Minor fixes.

--- src/test/java/com/example/demo/DemoJmockitApplicationTests.java
+++ src/test/java/com/example/demo/DemoJmockitApplicationTests.java
@@ -1,5 +1,7 @@
 package com.example.demo;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.slf4j.Logger;
@@ -14,23 +16,46 @@ import mockit.Mocked;
 @SpringBootTest
 public class DemoJmockitApplicationTests {
 
-	private static Logger log = LoggerFactory.getLogger(DemoJmockitApplicationTests.class);
-	
-	@Mocked
-	ClassB classB;
-	
-	@Test
-	public void testMockedWithNewOperatorInstance() {
-		
-		new Expectations() {{
-	        classB.getGrettings(); result = "Hello from mocked ClassB";
-	    }};
-	    
-		ClassA classA = new ClassA();
-		
-		log.info("ClassA: {}", classA.getMessage());
-		log.info("ClassA type: {}", classA.getType());
-		
-	}
+    private static Logger log = LoggerFactory.getLogger(DemoJmockitApplicationTests.class);
+
+    @Mocked
+    private ClassB classB;
+
+    
+    @Test
+    public void testMockedWithNewOperatorInstance1() {
+
+        new Expectations() {
+            {
+                classB.getGrettings();
+                result = "Hello from mocked ClassB - First try ...";
+            }
+        };
+
+        ClassA classA = new ClassA();
+
+        log.info("ClassA: {}", classA.getMessage());
+        log.info("ClassA type: {}", classA.getType());
+
+    }
+
+    @Test
+    public void testMockedWithNewOperatorInstance2() {
+
+        new Expectations() {
+            {
+                classB.getGrettings();
+                result = "Hello from mocked ClassB - Second try ...";
+            }
+        };
+
+        ClassA classA = new ClassA();
+
+        log.info("ClassA: {}", classA.getMessage());
+        log.info("ClassA type: {}", classA.getType());
+        
+        assertEquals("Hello from mocked ClassB - Second try ...", classA.getMessage());
+
+    }
 
 }
